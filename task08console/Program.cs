170e34116f4e9d4ced0c30e7bae5b1cb4f3b8251
ARKOD-PM <konstantinarkashev@gmail.com>
2025-07-16 02:57:48 +0000

    fix(task08): resolve assembly loading path
    - Replaced hardcoded assembly load with dynamic path resolution via AppDomain.CurrentDomain.BaseDirectory to match reality.
    - Renamed target assembly from FileSystemCommands.dll to task08.dll to match reality.

--- task08console/Program.cs
+++ task08console/Program.cs
@@ -4,59 +4,61 @@ using CommandLib;
 
 namespace CommandRunner
 {
-    class Program
-    {
-        static void Main()
-        {
-            var assembly = Assembly.LoadFrom("FileSystemCommands.dll");
-            
-            foreach (var type in assembly.GetTypes())
-            {
-                if (typeof(ICommand).IsAssignableFrom(type) && !type.IsInterface)
-                {
-                    Console.WriteLine($"Вызывается следующая команда: {type.Name}");
-                    
-                    ICommand command = CreateCommandInstance(type);
-                    
-                    command.Execute();
-                    
-                    PrintCommandResults(command);
-                }
-            }
-        }
+	class Program
+	{
+		static void Main()
+		{
+			
+			string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "task08.dll");
+			var assembly = Assembly.LoadFrom(path);
+			
+			foreach (var type in assembly.GetTypes())
+			{
+				if (typeof(ICommand).IsAssignableFrom(type) && !type.IsInterface)
+				{
+					Console.WriteLine($"Вызывается следующая команда: {type.Name}");
+					
+					ICommand command = CreateCommandInstance(type);
+					
+					command.Execute();
+					
+					PrintCommandResults(command);
+				}
+			}
+		}
 
-        private static ICommand CreateCommandInstance(Type commandType)
-        {
-            var constructors = commandType.GetConstructors();
-            var parameters = constructors[0].GetParameters();
-            var arguments = new object[parameters.Length];
+		private static ICommand CreateCommandInstance(Type commandType)
+		{
+			var constructors = commandType.GetConstructors();
+			var parameters = constructors[0].GetParameters();
+			var arguments = new object[parameters.Length];
 
-            for (int i = 0; i < parameters.Length; i++)
-            {
-                Console.Write($"Введите {parameters[i].Name}: ");
-                arguments[i] = Console.ReadLine();
-            }
+			for (int i = 0; i < parameters.Length; i++)
+			{
+				Console.Write($"Введите {parameters[i].Name}: ");
+				arguments[i] = Console.ReadLine();
+			}
 
-            return (ICommand)Activator.CreateInstance(commandType, arguments);
-        }
+			return (ICommand)Activator.CreateInstance(commandType, arguments);
+		}
 
-        private static void PrintCommandResults(ICommand command)
-        {
-            switch (command)
-            {
-                case DirectorySizeCommand sizeCmd:
-                    Console.WriteLine($"Итоговый размер: {sizeCmd.TotalSize} байт");
-                    break;
-                
-                case FindFilesCommand filesCmd:
-                    Console.WriteLine("Найдены следующие файлы:");
-                    foreach (var file in filesCmd.FoundFiles)
-                    {
-                        Console.WriteLine($"- {file}");
-                    }
-                    break;
-            }
-            Console.WriteLine();
-        }
-    }
+		private static void PrintCommandResults(ICommand command)
+		{
+			switch (command)
+			{
+				case DirectorySizeCommand sizeCmd:
+					Console.WriteLine($"Итоговый размер: {sizeCmd.TotalSize} байт");
+					break;
+				
+				case FindFilesCommand filesCmd:
+					Console.WriteLine("Найдены следующие файлы:");
+					foreach (var file in filesCmd.FoundFiles)
+					{
+						Console.WriteLine($"- {file}");
+					}
+					break;
+			}
+			Console.WriteLine();
+		}
+	}
 }
